24457a57fb48322896fbbfda80e0885b656435e4
Soumen Khara <soumen2015.s.k@gmail.com>
2021-12-16 18:07:00 +0530

    Adding ethereum contract

---
+++ truffle-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    contracts_build_directory: "./client/src/artifact",
+    networks: {},
+    mocha: {},
+    compilers: {
+        solc: {
+            version: "0.8.10",
+        },
+    },
+};
