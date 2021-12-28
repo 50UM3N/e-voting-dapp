834ccc4b624c7612c7ef6089c2fd00cfd9d457d8
Soumen Khara <soumen2015.s.k@gmail.com>
2021-12-28 18:17:09 +0530

    Adding ganache network

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,13 @@
 module.exports = {
     contracts_build_directory: "./client/src/artifact",
-    networks: {},
+    networks: {
+        development: {
+            // from: "", // Defaults to first address from Ganache
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: "*",
+        },
+    },
     mocha: {},
     compilers: {
         solc: {
