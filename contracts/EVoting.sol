a70a8751fdffd7bc7c3979eb101bd78ca959a0aa
Soumen Khara <soumen2015.s.k@gmail.com>
2022-01-01 21:41:13 +0530

    feat: Added new features in the solidity file

--- contracts/EVoting.sol
+++ contracts/EVoting.sol
@@ -3,17 +3,24 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract evoting {
     struct Voter {
-        bool added;
+        bytes32 fname;
+        bytes32 lname;
+        bytes32 email;
+        uint256 dob;
+        bytes32 mobile;
+        bytes32 uidai;
+        bytes32 role;
+        bool verified;
         bool voted;
         uint256 vote;
     }
 
     struct Team {
-        bytes32 name;
+        bytes32 representative;
+        bytes32 description;
+        bytes32 teamName;
         uint256 voteCount;
     }
-
-    address public admin;
 
     mapping(address => Voter) public voters;
 
@@ -21,68 +28,104 @@ contract evoting {
 
     event AddVoter(Voter _voter);
 
-    constructor(bytes32[] memory teamsName) {
-        admin = msg.sender;
-        voters[admin].added = true;
-        for (uint256 i = 0; i < teamsName.length; i++) {
-            teams.push(Team({name: teamsName[i], voteCount: 0}));
-        }
+    constructor(
+        bytes32 fname,
+        bytes32 lname,
+        bytes32 email,
+        uint256 dob,
+        bytes32 mobile,
+        bytes32 uidai
+    ) {
+        address admin = msg.sender;
+        voters[admin].fname = bytes32(fname);
+        voters[admin].lname = bytes32(lname);
+        voters[admin].email = bytes32(email);
+        voters[admin].dob = dob;
+        voters[admin].mobile = bytes32(mobile);
+        voters[admin].uidai = bytes32(uidai);
+        voters[admin].verified = true;
+        voters[admin].role = bytes32("admin");
     }
 
-    function addVoter(address voter) public {
-        require(
-            msg.sender == admin,
-            "Only admin can add new voter to voter list."
-        );
-        require(
-            !voters[voter].added,
-            "Voter is already added in the voter list"
-        );
-        voters[voter].added = true;
+    event AddTeam(Team _team);
 
-        emit AddVoter(voters[voter]);
+    function addTeam(
+        bytes32 representative,
+        bytes32 description,
+        bytes32 teamName
+    ) public {
+        require(
+            voters[msg.sender].role == "admin",
+            "Only admin can add new teams"
+        );
+        teams.push(
+            Team({
+                representative: representative,
+                description: description,
+                teamName: teamName,
+                voteCount: 0
+            })
+        );
+        emit AddTeam(teams[teams.length - 1]);
+    }
+
+    event Register(Voter _voter);
+
+    function register(
+        bytes32 fname,
+        bytes32 lname,
+        bytes32 email,
+        uint256 dob,
+        bytes32 mobile,
+        bytes32 uidai
+    ) public {
+        address voter = msg.sender;
+        // check that a user is already exist or not;
+        voters[voter].fname = fname;
+        voters[voter].lname = lname;
+        voters[voter].email = email;
+        voters[voter].dob = dob;
+        voters[voter].mobile = mobile;
+        voters[voter].uidai = uidai;
+        voters[voter].verified = false;
+        voters[voter].role = "user";
+
+        emit Register(voters[voter]);
     }
 
     function getTeams() public view returns (Team[] memory) {
         return teams;
     }
 
-    function getUser() public view returns (Voter memory) {
+    function getVoter() public view returns (Voter memory) {
         return voters[msg.sender];
-    }
-
-    function isAdmin() public view returns (bool) {
-        if (msg.sender == admin) return true;
-        else return false;
     }
 
     event Vote(Voter _voter);
 
     function vote(uint256 to) public {
         Voter storage voter = voters[msg.sender];
-        require(voter.added, "You are not eligible to give vote.");
 
+        require(voter.verified, "You are not eligible to give vote.");
         require(!voter.voted, "You are already voted.");
 
         voter.voted = true;
         voter.vote = to;
-
         teams[to].voteCount += 1;
-
         emit Vote(voter);
     }
 
-    function getWinnerIndex() public view returns (uint256 _winner) {
-        uint256 maxVote = 0;
-        for (uint256 i = 0; i < teams.length; i++) {
-            if (teams[i].voteCount > maxVote) {
-                maxVote = teams[i].voteCount;
-                _winner = i;
-            }
-        }
-    }
+    // function getWinnerIndex() public view returns (uint256 _winner) {
+    //     uint256 maxVote = 0;
+    //     for (uint256 i = 0; i < teams.length; i++) {
+    //         if (teams[i].voteCount > maxVote) {
+    //             maxVote = teams[i].voteCount;
+    //             _winner = i;
+    //         }
+    //     }
+    // }
 
-    function getWinner() public view returns (bytes32 _winner) {
-        _winner = teams[getWinnerIndex()].name;
-    }
+    // function getWinner() public view returns (bytes32 _winner) {
+    //     _winner = teams[getWinnerIndex()].name;
+    // }
 }
