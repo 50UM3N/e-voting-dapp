9b396f0d9377f9b9621d04037f20c88b3c5a5892
Soumen Khara <soumen2015.s.k@gmail.com>
2022-03-12 22:48:21 +0530

    feat: implemented voter request

--- client/src/Routes/Request.js
+++ client/src/Routes/Request.js
@@ -1,67 +1,95 @@
 import { useEffect, useState } from "react";
+import { connect } from "react-redux";
 import Navbar from "../Components/Navbar";
-
-const Request = () => {
+import Moment from "react-moment";
+const Request = ({ contract }) => {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     useEffect(() => {
+        (async () => {
+            let accounts = window.ethereum.request({
+                method: "eth_accounts",
+            });
+            let voters = await contract.contract.methods
+                .getUnverifiedVoter()
+                .call({ from: accounts[5] })
+                .catch((err) => {
+                    setError(err);
+                });
+            setData(voters);
+            setLoading(false);
+        })();
         return () => {};
-    }, []);
-
+    }, [contract]);
+    console.log(data);
     return (
         <>
             <Navbar />
-            <div className="container my-5">
-                <div className="card">
-                    <div className="card-body">
-                        <div className="table-responsive">
-                            <table className="table table-bordered mb-0">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">First Name</th>
-                                        <th scope="col">Last Name</th>
-                                        <th scope="col">Email Address</th>
-                                        <th scope="col">Mobile Number</th>
-                                        <th scope="col">DOB</th>
-                                        <th scope="col">Aadhar Id</th>
-                                        <th scope="col">Rooms</th>
-                                        <th scope="col"></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {/* {hotels.map((item, index) => (
-                                        <tr
-                                            key={item.id}
-                                            className="text-capitalize text-nowrap"
-                                        >
-                                            <th scope="row">{item.id}</th>
-                                            <td>{item.hotel_initial}</td>
-                                            <td>{item.hotel_type}</td>
-                                            <td>{item.hotel_name}</td>
-                                            <td>{item.hotel_district}</td>
-                                            <td>{item.hotel_state}</td>
-                                            <td>{item.hotel_place}</td>
-                                            <td>{item.rooms.length}</td>
-                                            <td>
-                                                <div className="btn-group">
-                                                    {computedComponent(
-                                                        item,
-                                                        index
-                                                    )}
-                                                </div>
-                                            </td>
+            {loading && <p>Loading...</p>}
+            {error && <p>{error}</p>}
+            {data && (
+                <div className="container my-5">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">New Voter Request</h5>
+                            <hr />
+                            <div className="table-responsive">
+                                <table className="table table-bordered mb-0">
+                                    <thead>
+                                        <tr className="text-capitalize text-nowrap">
+                                            <th scope="col">#</th>
+                                            <th scope="col">Aadhar Id</th>
+                                            <th scope="col">First Name</th>
+                                            <th scope="col">Last Name</th>
+                                            <th scope="col">Email Address</th>
+                                            <th scope="col">Mobile Number</th>
+                                            <th scope="col">DOB</th>
+                                            <th scope="col"></th>
                                         </tr>
-                                    ))} */}
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                        {data.map((item) => (
+                                            <tr
+                                                key={item.id}
+                                                className="text-nowrap"
+                                            >
+                                                <th scope="row">{item.id}</th>
+                                                <td>{item.uidai}</td>
+                                                <td>{item.fname}</td>
+                                                <td>{item.lname}</td>
+                                                <td>{item.email}</td>
+                                                <td>{item.mobile}</td>
+                                                <td>
+                                                    <Moment format="YYYY/MM/DD">
+                                                        {
+                                                            new Date(
+                                                                Number(item.dob)
+                                                            )
+                                                        }
+                                                    </Moment>
+                                                </td>
+                                                <td>
+                                                    <button className="btn btn-sm btn-primary">
+                                                        Approved
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            )}
         </>
     );
 };
 
-export default Request;
+const mapStateToProps = (state) => {
+    return {
+        contract: state.contractReducer,
+    };
+};
+export default connect(mapStateToProps)(Request);
--- contracts/EVoting.sol
+++ contracts/EVoting.sol
@@ -2,39 +2,39 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract evoting {
+    
     /**
-     * Voting structure that helps to create a map and contain all the voter
-     * thar are register
-     */
+    * Voting structure that helps to create a map and contain all the voter
+    * thar are register
+    */
     struct Voter {
-        string fname; // First Name
-        string lname; // Last name
-        string email; // Email Address
-        uint256 dob; // Date of Birth as integer to store the date time according to JS date time
-        string mobile; // Mobile Number
-        string uidai; // Aadhar Number
-        string role; // Role i.e user or admin
-        bool verified; // Verified flag that help to verifiy the voter details
-        bool voted; // Voted flag to verify that the user give his vote or not
-        uint256 vote; // Index of the voter list the user give vote
+        string fname;   // First Name
+        string lname;   // Last name
+        string email;   // Email Address
+        uint256 dob;    // Date of Birth as integer to store the date time according to JS date time
+        string mobile;  // Mobile Number
+        string uidai;   // Aadhar Number
+        string role;    // Role i.e user or admin
+        bool verified;  // Verified flag that help to verifiy the voter details
+        bool voted;     // Voted flag to verify that the user give his vote or not
+        uint256 vote;   // Index of the voter list the user give vote
     }
 
     struct VoterMap {
         address[] keys;
         mapping(address => Voter) values;
-        mapping(address => uint256) indexOf;
+        mapping(address => uint) indexOf;
         mapping(address => bool) inserted;
     }
 
     VoterMap private voterMap;
-
     /**
-     * voterMapGet function get the voter of a esisting address
-     * @param {VoterMap} map is the storage parameter
-     * @param {address} key This is the key to find a voter
-     * @return {Voter} return the structure voter
-     */
-    function voterMapSize() private view returns (uint256) {
+    * voterMapGet function get the voter of a esisting address
+    * @param {VoterMap} map is the storage parameter
+    * @param {address} key This is the key to find a voter
+    * @return {Voter} return the structure voter
+    */
+    function voterMapSize() private view returns (uint) {
         return voterMap.keys.length;
     }
 
@@ -49,7 +49,7 @@ contract evoting {
         }
     }
 
-    function voterMapRemove(address key) private {
+    function voterMapRemove( address key) private {
         if (!voterMap.inserted[key]) {
             return;
         }
@@ -57,8 +57,8 @@ contract evoting {
         delete voterMap.inserted[key];
         delete voterMap.values[key];
 
-        uint256 index = voterMap.indexOf[key];
-        uint256 lastIndex = voterMap.keys.length - 1;
+        uint index = voterMap.indexOf[key];
+        uint lastIndex = voterMap.keys.length - 1;
         address lastKey = voterMap.keys[lastIndex];
 
         voterMap.indexOf[lastKey] = index;
@@ -75,88 +75,43 @@ contract evoting {
     address admin;
 
     /**
-     * Team structure that helps to create an array contains all the teams
-     */
+    * Team structure that helps to create an array contains all the teams 
+    */
     struct Team {
-        string representative; // Name of the representative who stand in behalf to to team
-        string description; // Description of the team
-        string teamName; // Team name
-        uint256 voteCount; // Number of vote
+        string representative;  // Name of the representative who stand in behalf to to team
+        string description;     // Description of the team
+        string teamName;        // Team name
+        uint256 voteCount;      // Number of vote 
     }
 
-    // Teams array contains all the teams
+    // Teams array contains all the teams 
     Team[] public teams;
 
     event AddVoter(Voter _voter);
-
     //"Soumen", "Khara", "soumen@gmail.com", 931113000000,"8945612397","12345678900"
-    constructor(
-        string memory fname,
-        string memory lname,
-        string memory email,
-        uint256 dob,
-        string memory mobile,
-        string memory uidai
-    ) {
+    constructor(string memory fname,string memory lname,string memory email,uint256 dob,string memory mobile,string memory uidai) {
         admin = msg.sender;
-        Voter memory voter = Voter(
-            fname,
-            lname,
-            email,
-            dob,
-            mobile,
-            uidai,
-            "admin",
-            true,
-            false,
-            0
-        );
+        Voter memory voter = Voter(fname,lname,email,dob,mobile,uidai,"admin",true, false, 0);
         voterMapSet(admin, voter);
     }
 
     event AddTeam(Team _team);
 
-    function addTeam(
-        string memory representative,
-        string memory description,
-        string memory teamName
-    ) public {
-        require(msg.sender == admin, "Only admin can add new teams");
-        teams.push(
-            Team({
-                representative: representative,
-                description: description,
-                teamName: teamName,
-                voteCount: 0
-            })
-        );
-        emit AddTeam(teams[teams.length - 1]);
+    function addTeam(string memory  representative, string  memory description,    string memory teamName) public {
+            require(
+                msg.sender == admin,
+                "Only admin can add new teams"
+            );
+            teams.push(Team({representative: representative, description: description,teamName:teamName,voteCount: 0}));
+            emit AddTeam(teams[teams.length - 1]);
     }
 
     event Register(Voter _voter);
 
-    function register(
-        string memory fname,
-        string memory lname,
-        string memory email,
-        uint256 dob,
-        string memory mobile,
-        string memory uidai
-    ) public {
+    function register(string  memory  fname,string  memory  lname,string  memory email,uint256 dob,string memory  mobile,string memory  uidai) public {
         address sender = msg.sender;
         // TODO: check that a user is already exist or not
-        Voter memory voter = Voter(
-            fname,
-            lname,
-            email,
-            dob,
-            mobile,
-            uidai,
-            "user",
-            false,
-            false,
-            0
-        );
+        Voter memory voter = Voter(fname,lname,email,dob,mobile,uidai,"user",false, false, 0);
         voterMapSet(sender, voter);
         emit Register(voter);
     }
@@ -166,6 +121,7 @@ contract evoting {
     }
 
     function getVoter() public view returns (Voter memory) {
+        
         return voterMap.values[msg.sender];
     }
 
@@ -183,17 +139,31 @@ contract evoting {
         emit Vote(voter);
     }
 
+
     function getUnverifiedVoter() public view returns (Voter[] memory) {
-        // TODO: Create a pagination type data return
-        // INFO: Resolve null value from the voters
-        Voter[] memory voters = new Voter[](voterMap.keys.length);
-        for (uint256 i = 0; i < voterMap.keys.length; i++) {
+        // TODO: Create a pagination type data return 
+        // INFO: Resolve null value from the voters 
+        uint counter = 0 ;
+
+        for(uint i=0; i<voterMap.keys.length;i++){
             Voter memory item = voterMap.values[voterMap.keys[i]];
-            if (!item.verified) voters[i] = item;
+            if(!item.verified)
+                counter++;
+        }
+
+        Voter[] memory voters = new Voter[](counter);
+        uint j = 0; 
+        for(uint i=0; i<voterMap.keys.length;i++){
+            Voter memory item = voterMap.values[voterMap.keys[i]];
+            if(!item.verified){
+                voters[j] = item;
+                j++;    
+            }
         }
         return voters;
     }
 
+    
     // function getWinnerIndex() public view returns (uint256 _winner) {
     //     uint256 maxVote = 0;
     //     for (uint256 i = 0; i < teams.length; i++) {
